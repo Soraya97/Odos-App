fa46e027e18bb9d236c293884ef7f780eecccbdc
Soraya <soraya.meystre@heig-vd.ch>
2020-12-18 14:08:32 +0100

    Delete a route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,10 +14,6 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
 ];
 
 @NgModule({
