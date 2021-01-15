979b27f61edc9e91b5232df9f0d03665134f8b32
Soraya Meystre <soraya.meystre@gmail.com>
2021-01-15 13:48:34 +0100

    Profile user

--- src/app/layout/profile/profile.page.ts
+++ src/app/layout/profile/profile.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from "../../models/user";
-import { UserService } from 'src/app/services/user.service';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,20 +10,18 @@ import { UserService } from 'src/app/services/user.service';
 export class ProfilePage implements OnInit {
   user: User;
 
-  constructor(private userService: UserService) {
-    // this.user = {_id: "1", username: "John Doe", email: "a@a.ch", password: "1234", registrationDate: "12.01.2021"};
+  constructor(private auth: AuthService) {
   }
 
-  addUser(): void {
-    this.userService.getUser().subscribe((user) => {
-      return this.user;
+  ngOnInit() {
+    this.auth.getUser().subscribe((user) => {
+      this.user = user;
     }, err => {
       console.warn(err);
       alert(err.message);
     });
-  }
 
-  ngOnInit() {
+
   }
 
 }
