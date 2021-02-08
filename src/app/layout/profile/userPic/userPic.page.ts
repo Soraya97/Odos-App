62fffb5838d842f1fb2b993f13fc720951f35e01
Soraya Meystre <soraya.meystre@gmail.com>
2021-02-08 11:13:29 +0100

    Double modals to delete something

--- src/app/layout/profile/userPic/userPic.page.ts
+++ src/app/layout/profile/userPic/userPic.page.ts
@@ -5,7 +5,7 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { PictureService } from 'src/app/services/picture.service';
 import { Picture } from 'src/app/models/pictures';
 
-import {ActionSheetController } from '@ionic/angular';
+import { ActionSheetController, AlertController } from '@ionic/angular';
 import { async } from '@angular/core/testing';
 import { User } from 'src/app/models/user';
 
@@ -19,57 +19,71 @@ export class userPicPage implements OnInit {
   picture: Picture;
   idPicture: string;
 
-  constructor(private auth: AuthService, private pictureService: PictureService, public actionsheetCtrl: ActionSheetController) {
+  constructor(private auth: AuthService, private pictureService: PictureService, public actionsheetCtrl: ActionSheetController, public alertController: AlertController) {
     let urlcourante = document.location.href;
     urlcourante = urlcourante.replace(/\/$/, "");
-    this.idPicture = urlcourante.substring (urlcourante.lastIndexOf( "/" )+1 );
+    this.idPicture = urlcourante.substring(urlcourante.lastIndexOf("/") + 1);
 
   }
 
-    async openMenuPic(){
-      const actionSheet = await this.actionsheetCtrl.create({
-        header: 'Picture options',
-        buttons: [
-          {
-            text: 'Delete',
-            role: 'destructive',
-            handler: () => {
-              let idPicture = "TO DO";
-              this.pictureService.deletePicture(idPicture).subscribe(
-              err => {
-                console.warn(err);
-                // alert(err.message);
-              });
-            }
-          },{
-            text: 'Modify',
-            role: 'modify',
-            handler: () => {
-              let idPicture = "TO DO";
-              let description = "TO DO";
-              this.pictureService.updatePicture(description, idPicture).subscribe(
-              err => {
-                console.warn(err);
-                // alert(err.message);
-              });
-            }
-          }, {
-            text: 'Cancel',
-            role: 'cancel',
-            handler: () => {
-              console.log('Cancel clicked');
-            }
+  async openMenuPic() {
+    const actionSheet = await this.actionsheetCtrl.create({
+      header: 'Picture options',
+      buttons: [
+        {
+          text: 'Delete',
+          role: 'destructive',
+          handler: () => {
+            console.log("Deleted");
+            this.presentAlert();
+            // let idPicture = "TO DO";
+            // this.pictureService.deletePicture(idPicture).subscribe(
+            // err => {
+            //   console.warn(err);
+            //   // alert(err.message);
+            // });
           }
-        ]
-      });
-      await actionSheet.present();
-    }
+        }, {
+          text: 'Modify',
+          role: 'modify',
+          handler: () => {
+            let idPicture = "TO DO";
+            let description = "TO DO";
+            this.pictureService.updatePicture(description, idPicture).subscribe(
+              err => {
+                console.warn(err);
+                // alert(err.message);
+              });
+          }
+        }, {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    await actionSheet.present();
+  }
 
-    getId(id){
-      console.log(id);
-      // TROUVER COMMENT REDIRIGER L'UTILISATEUR
-      // let selectedPicture: any = this.picture.filter((picture) => picture.id === id)[0];
-    }
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  getId(id) {
+    console.log(id);
+    // TROUVER COMMENT REDIRIGER L'UTILISATEUR
+    // let selectedPicture: any = this.picture.filter((picture) => picture.id === id)[0];
+  }
 
   ngOnInit() {
     this.auth.getUser().subscribe((user) => {
