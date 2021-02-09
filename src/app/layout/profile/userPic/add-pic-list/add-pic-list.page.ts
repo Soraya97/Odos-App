0cb87a2508d37c89615d2dc8279bfb77fca5e198
Soraya Meystre <soraya.meystre@gmail.com>
2021-02-09 19:32:46 +0100

    Update a list to add a picture

--- src/app/layout/profile/userPic/add-pic-list/add-pic-list.page.ts
+++ src/app/layout/profile/userPic/add-pic-list/add-pic-list.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ListService } from 'src/app/services/list.service';
 import { List } from 'src/app/models/list';
+import { ToastController } from '@ionic/angular';
+import { Picture } from 'src/app/models/pictures';
+import { PictureService } from 'src/app/services/picture.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-pic-list',
@@ -11,35 +15,67 @@ export class AddPicListPage implements OnInit {
   lists: List;
   ids: string[];
   idPicture: string;
+  picture: Picture;
+  list: string;
+  idList: string;
 
-  constructor(private listService: ListService) {
+  constructor(private listService: ListService, private pictureService: PictureService, public toastController: ToastController) {
 
     let urlcourante = document.location.href;
     urlcourante = urlcourante.replace(/\/$/, "");
     this.idPicture = urlcourante.substring(urlcourante.lastIndexOf("/") + 1);
     console.log(this.idPicture);
 
+    // this.idList = "602273117b36d20017fb416e";
   }
 
-  addPicsList() {
-    // for (let i = 0; i < 2; i++) {
-    //   let elements = document.getElementById(this.lists[i]._id);
-    //   elements = elements.checked;
-    //   elements = elements.value;
-    //   console.log(elements);
-    // }
-    let idList = "602273117b36d20017fb416e";
-    this.listService.updateList(undefined, this.idPicture, idList).subscribe(err => {
-      console.warn(err);
-      // alert(err.message);
-    });
+  addPicsList(form: NgForm) {
+    if (form.valid) {
 
+      for (let i = 0; i < 4; i++) {
+        let radioChecked = <HTMLInputElement>document.getElementById(this.lists[i]._id);
+
+        if (radioChecked.checked = true) {
+          console.log("It's checked");
+          // Get the name of list
+          let nameList = radioChecked.value;
+          // Get the id of list
+          let idList = radioChecked.name;
+          this.list = nameList;
+          this.idList = idList;
+        } else {
+          console.log("Not checked");
+        }
+
+      }
+      // this.listService.updateList(undefined, this.idPicture, this.idList).subscribe(err => {
+      //   console.warn(err);
+      //   // alert(err.message);
+      // });
+      this.addToListToast();
+    }
+  }
+
+  async addToListToast() {
+    const toast = await this.toastController.create({
+      message: `La photo ${this.picture.description} a bien été ajoutée à la liste ${this.list}`,
+      duration: 2000
+    });
+    toast.present();
   }
 
   ngOnInit() {
     this.listService.getAllLists().subscribe((list) => {
       this.lists = list;
       console.log(this.lists);
+    }, err => {
+      console.warn(err);
+      alert(err.message);
+    });
+
+
+    this.pictureService.getPicture(this.idPicture).subscribe((picture) => {
+      this.picture = picture;
     }, err => {
       console.warn(err);
       alert(err.message);
