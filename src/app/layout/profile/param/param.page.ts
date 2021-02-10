d76d6006c9e36ef6eac83ab80e91431d155a71d0
Leyna Girardi <leyna.girardi@heig-vd.ch>
2021-02-10 23:28:32 +0100

    Add error management in profile, userPic, param

--- src/app/layout/profile/param/param.page.ts
+++ src/app/layout/profile/param/param.page.ts
@@ -3,10 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { User } from "../../../models/user";
 import { AuthService } from 'src/app/auth/auth.service';
 
-import { ActionSheetController, AlertController, ToastController } from '@ionic/angular';
-
-import { PopoverController } from '@ionic/angular';
-// import { PopoverComponent } from '../../component/popover/popover.component';
+import { ActionSheetController, AlertController, ToastController, PopoverController } from '@ionic/angular';
 
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
@@ -22,7 +19,7 @@ export class ParamPage implements OnInit {
   username: string;
   email: string;
   password: string;
-  idUser: string;
+  // idUser: string;
   editable: boolean;
   notEditable: boolean;
 
@@ -30,60 +27,7 @@ export class ParamPage implements OnInit {
     this.notEditable = true;
   }
 
-  //test formulaire à suprimmer
-  hello(username: string): string {
-    return `Bonjour ${username} !`;
-  }
-
-  paramForm() {
-    console.log(this.username);
-    console.log(this.email);
-  }
-
-  async presentAlertConfirm() {
-    const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
-      header: 'Attention!',
-      message: 'Êtes-vous sûr·e de vouloir supprimer votre compte?',
-      buttons: [
-        {
-          text: 'Annuler',
-          role: 'cancel',
-          cssClass: 'secondary',
-          handler: (non) => {
-            console.log('Confirm Cancel: non');
-          }
-        }, {
-          text: 'Confirmer',
-          handler: () => {
-            console.log('Confirm Yes: oui');
-            this.userService.deleteUser().subscribe(
-              err => {
-                console.warn(err);
-                // alert(err.message);
-              });
-            this.deletedUserToast();
-            console.log("logging out...");
-            this.auth.logOut();
-            this.router.navigateByUrl("login");
-
-          }
-        }
-      ]
-    });
-
-    await alert.present();
-  }
-
-  // Confirmation that the user is deleted
-  async deletedUserToast() {
-    const toast = await this.toastController.create({
-      message: 'Votre compte a été supprimé',
-      duration: 2000
-    });
-    toast.present();
-  }
-
+  // Open the menu to modify the user's information
   async openMenuPic() {
     const actionSheet = await this.actionsheetCtrl.create({
       header: 'Options du compte',
@@ -119,6 +63,7 @@ export class ParamPage implements OnInit {
     this.notEditable = true;
   }
 
+  // Alert to confirm if the user want to proceed to the update
   async updateConfirm(form: NgForm) {
     const alert = await this.alertController.create({
       cssClass: 'my-custom-class',
@@ -130,15 +75,15 @@ export class ParamPage implements OnInit {
           text: 'Annuler',
           role: 'cancel',
           cssClass: 'secondary',
-          handler: (non) => {
+          handler: () => {
             console.log('Confirm Cancel: non');
             this.notEditUser();
           }
         }, {
           text: 'Modifier',
           handler: () => {
-            this.saveUserUpdated(form);
-          }
+            this.saveUserUpdated(form );
+          },
         }
       ]
     });
@@ -156,35 +101,65 @@ export class ParamPage implements OnInit {
       let username = this.username;
       let email = this.email;
       let password = this.password;
-      this.userService.updateUser(username, email, password).subscribe();
+      this.userService.updateUser(username, email, password).subscribe(() =>{
+        console.log("logging out...");
+        this.auth.logOut();
+        this.router.navigateByUrl("login");
+        this.toast("Veuillez vous reconnecter");
+      },
+      (err) => {
+        console.warn(err);
+        this.toast("Les modifications n'ont pas pu être apportées");
 
-      console.log("logging out...");
-      this.auth.logOut();
-      this.router.navigateByUrl("login");
-      this.logOutToast();
+      });
     }
   }
 
-  async logOutToast() {
-    const toast = await this.toastController.create({
-      message: 'Veuillez vous reconnecter',
-      duration: 2000
+  // Alert to confirm if the user want to delete her/his account
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Attention!',
+      message: 'Êtes-vous sûr·e de vouloir supprimer votre compte?',
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (non) => {
+            console.log('Confirm Cancel: non');
+          }
+        }, {
+          text: 'Confirmer',
+          handler: () => {
+            console.log('Confirm Yes: oui');
+            this.userService.deleteUser().subscribe(() => {
+              this.toast("Votre compte a été supprimé");
+              console.log("logging out...");
+              this.auth.logOut();
+              this.router.navigateByUrl("login");
+            }, 
+            (err) => {
+              console.warn(err);
+              this.toast("Le compte n'a pas pu être supprimé");
+            });
+          }
+        }
+      ]
     });
-    toast.present();
+    await alert.present();
   }
 
-  // async presentPopover(ev: any) {
-  //   const popover = await this.popoverController.create({
-  //     component: PopoverComponent,
-  //     cssClass: 'my-custom-class',
-  //     event: ev,
-  //     translucent: true
-  //   });
-  //   return await popover.present();
-  // }
+    // Display a message
+    async toast(msg) {
+      const toast = await this.toastController.create({
+        message: msg,
+        duration: 2000
+      });
+      toast.present();
+    }
 
   // Open the menu of options: Delete or Update
-
   ngOnInit() {
     this.auth.getUser().subscribe((user) => {
       this.user = user;
